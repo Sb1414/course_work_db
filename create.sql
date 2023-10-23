f62508152aeac9fb4a3a3ffef03ca1e8c260ad33
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-10-23 16:50:30 +0300

    create table

---
+++ create.sql
@@ -0,0 +1,38 @@
+CREATE TABLE "Attractions" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"Name"	TEXT NOT NULL,
+	"Description"	TEXT NOT NULL,
+	"Capacity"	INTEGER NOT NULL,
+	"TicketPrice"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "Tickets" (
+	"Id"	INTEGER NOT NULL UNIQUE,
+	"PurchaseDate"	DATE NOT NULL,
+	"AttractionID"	INTEGER NOT NULL,
+	PRIMARY KEY("Id" AUTOINCREMENT),
+	FOREIGN KEY("AttractionID") REFERENCES "Attractions"("id")
+);
+
+CREATE TABLE "Employees" (
+	"Id"	INTEGER NOT NULL UNIQUE,
+	"Login"	TEXT NOT NULL UNIQUE,
+	"Password"	TEXT NOT NULL,
+	"FirstName"	TEXT NOT NULL,
+	"LastName"	TEXT NOT NULL,
+	"DateOfBirth"	DATE NOT NULL,
+	PRIMARY KEY("Id" AUTOINCREMENT)
+);
+
+CREATE TABLE "Shifts" (
+	"Id"	INTEGER NOT NULL UNIQUE,
+	"EmployeeID"	INTEGER NOT NULL UNIQUE,
+	"StartTime"	DateTime NOT NULL,
+	"EndTime"	DATETIME NOT NULL,
+	"Position"	TEXT NOT NULL,
+	"Salary"	INTEGER NOT NULL,
+	FOREIGN KEY("EmployeeID") REFERENCES "Employees"("Id"),
+	PRIMARY KEY("Id" AUTOINCREMENT)
+);
+
