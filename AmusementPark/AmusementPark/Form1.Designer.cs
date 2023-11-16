f420fb4cec131472d47e01849af64a3c3a8322e1
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-11-16 22:43:33 +0300

    hui na

---
+++ AmusementPark/AmusementPark/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace AmusementPark
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxLogin = new System.Windows.Forms.TextBox();
+			this.textBoxPassword = new System.Windows.Forms.TextBox();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.panelBack = new System.Windows.Forms.Panel();
+			this.labelInfo = new System.Windows.Forms.Label();
+			this.panelUp = new System.Windows.Forms.Panel();
+			this.buttonClose = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.panelBack.SuspendLayout();
+			this.panelUp.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBoxLogin
+			// 
+			this.textBoxLogin.Location = new System.Drawing.Point(139, 73);
+			this.textBoxLogin.Name = "textBoxLogin";
+			this.textBoxLogin.Size = new System.Drawing.Size(214, 22);
+			this.textBoxLogin.TabIndex = 0;
+			this.textBoxLogin.TextChanged += new System.EventHandler(this.textBoxLogin_TextChanged);
+			// 
+			// textBoxPassword
+			// 
+			this.textBoxPassword.Location = new System.Drawing.Point(139, 142);
+			this.textBoxPassword.Name = "textBoxPassword";
+			this.textBoxPassword.Size = new System.Drawing.Size(214, 22);
+			this.textBoxPassword.TabIndex = 1;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Location = new System.Drawing.Point(139, 208);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(97, 37);
+			this.btnLogin.TabIndex = 2;
+			this.btnLogin.Text = "Войти";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(256, 208);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(97, 37);
+			this.btnClear.TabIndex = 3;
+			this.btnClear.Text = "Очистить";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// panelBack
+			// 
+			this.panelBack.Controls.Add(this.dataGridView1);
+			this.panelBack.Controls.Add(this.labelInfo);
+			this.panelBack.Controls.Add(this.panelUp);
+			this.panelBack.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelBack.Location = new System.Drawing.Point(0, 0);
+			this.panelBack.Name = "panelBack";
+			this.panelBack.Size = new System.Drawing.Size(919, 452);
+			this.panelBack.TabIndex = 4;
+			// 
+			// labelInfo
+			// 
+			this.labelInfo.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.labelInfo.Location = new System.Drawing.Point(136, 167);
+			this.labelInfo.Name = "labelInfo";
+			this.labelInfo.Size = new System.Drawing.Size(217, 23);
+			this.labelInfo.TabIndex = 1;
+			// 
+			// panelUp
+			// 
+			this.panelUp.Controls.Add(this.buttonClose);
+			this.panelUp.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelUp.Location = new System.Drawing.Point(0, 0);
+			this.panelUp.Name = "panelUp";
+			this.panelUp.Size = new System.Drawing.Size(919, 27);
+			this.panelUp.TabIndex = 0;
+			this.panelUp.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelUp_MouseDown);
+			this.panelUp.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelUp_MouseMove);
+			// 
+			// buttonClose
+			// 
+			this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.buttonClose.Dock = System.Windows.Forms.DockStyle.Left;
+			this.buttonClose.FlatAppearance.BorderSize = 0;
+			this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonClose.Image = global::AmusementPark.Properties.Resources.icons8_macos_закрыть_20__1_;
+			this.buttonClose.Location = new System.Drawing.Point(0, 0);
+			this.buttonClose.Name = "buttonClose";
+			this.buttonClose.Size = new System.Drawing.Size(27, 27);
+			this.buttonClose.TabIndex = 0;
+			this.buttonClose.UseVisualStyleBackColor = true;
+			this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(376, 33);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(531, 407);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(919, 452);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.textBoxPassword);
+			this.Controls.Add(this.textBoxLogin);
+			this.Controls.Add(this.panelBack);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Вход";
+			this.panelBack.ResumeLayout(false);
+			this.panelUp.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxLogin;
+		private System.Windows.Forms.TextBox textBoxPassword;
+		private System.Windows.Forms.Button btnLogin;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Panel panelBack;
+		private System.Windows.Forms.Panel panelUp;
+		private System.Windows.Forms.Button buttonClose;
+		private System.Windows.Forms.Label labelInfo;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ AmusementPark/AmusementPark/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace AmusementPark
+{
+	public partial class Form1 : Form
+	{
+		string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+		public Form1()
+		{
+			InitializeComponent();
+			LoadUserData();
+		}
+
+		private void LoadUserData()
+		{
+			using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+			{
+				connection.Open();
+
+				string query = "SELECT * FROM Employees";
+
+				using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, connection))
+				{
+					DataTable dataTable = new DataTable();
+					adapter.Fill(dataTable);
+
+					// Assuming you have a DataGridView named dataGridView1 on your form
+					dataGridView1.DataSource = dataTable;
+				}
+			}
+		}
+
+		private void buttonClose_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		Point lastPoint;
+		private void panelUp_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.Left += e.X - lastPoint.X;
+				this.Top += e.Y - lastPoint.Y;
+			}
+		}
+
+		private void panelUp_MouseDown(object sender, MouseEventArgs e)
+		{
+			lastPoint = new Point(e.X, e.Y);
+		}
+
+		private void btnLogin_Click(object sender, EventArgs e)
+		{
+			string username = textBoxLogin.Text;
+			string password = textBoxPassword.Text;
+
+			if (IsValidLogin(username, password))
+			{
+				MessageBox.Show("Login successful!");
+			}
+			else
+			{
+				MessageBox.Show("Invalid login credentials. Please try again.");
+			}
+		}
+
+		private bool IsValidLogin(string username, string password)
+		{
+			using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+			{
+				try
+				{
+					connection.Open();
+					Console.WriteLine("Connected to PostgreSQL database!");
+
+					string query = "SELECT COUNT(*) FROM UserCredentials WHERE Login = @Username AND Password = @Password";
+					using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+					{
+						command.Parameters.AddWithValue("@Username", username);
+						command.Parameters.AddWithValue("@Password", password);
+
+						int count = (int)command.ExecuteScalar();
+						return count > 0;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error: {ex.Message}");
+					return false; // Return false in case of an exception
+				}
+			}
+		}
+
+
+		private void textBoxLogin_TextChanged(object sender, EventArgs e)
+		{
+			string currentText = textBoxLogin.Text;
+			string filteredText = new string(currentText.Where(c => Char.IsLetterOrDigit(c)).ToArray());
+
+			textBoxLogin.Text = filteredText;
+			textBoxLogin.SelectionStart = textBoxLogin.Text.Length;
+		}
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			textBoxLogin.Clear();
+			textBoxPassword.Clear();
+		}
+	}
+}
