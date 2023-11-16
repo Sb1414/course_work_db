9c76bdcad5b86d4795332ae744b4b0d10a2e02a3
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-11-16 21:42:18 +0300

    sql

--- create.sql
+++ create.sql
@@ -1,38 +1,58 @@
-CREATE TABLE "Attractions" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"Name"	TEXT NOT NULL,
-	"Description"	TEXT NOT NULL,
-	"Capacity"	INTEGER NOT NULL,
-	"TicketPrice"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
+CREATE TABLE UserCredentials (
+    Id SERIAL PRIMARY KEY,
+    Login TEXT NOT NULL UNIQUE,
+    Password TEXT NOT NULL
 );
 
-CREATE TABLE "Tickets" (
-	"Id"	INTEGER NOT NULL UNIQUE,
-	"PurchaseDate"	DATE NOT NULL,
-	"AttractionID"	INTEGER NOT NULL,
-	PRIMARY KEY("Id" AUTOINCREMENT),
-	FOREIGN KEY("AttractionID") REFERENCES "Attractions"("id")
+CREATE TABLE Attractions (
+     id SERIAL PRIMARY KEY,
+     Name TEXT NOT NULL,
+     Description TEXT NOT NULL,
+     Capacity INTEGER NOT NULL,
+     TicketPrice INTEGER NOT NULL
 );
 
-CREATE TABLE "Employees" (
-	"Id"	INTEGER NOT NULL UNIQUE,
-	"Login"	TEXT NOT NULL UNIQUE,
-	"Password"	TEXT NOT NULL,
-	"FirstName"	TEXT NOT NULL,
-	"LastName"	TEXT NOT NULL,
-	"DateOfBirth"	DATE NOT NULL,
-	PRIMARY KEY("Id" AUTOINCREMENT)
+CREATE TABLE Positions (
+    Id SERIAL PRIMARY KEY,
+    Position TEXT NOT NULL,
+    Salary INTEGER NOT NULL,
+    Count INTEGER NOT NULL
 );
 
-CREATE TABLE "Shifts" (
-	"Id"	INTEGER NOT NULL UNIQUE,
-	"EmployeeID"	INTEGER NOT NULL UNIQUE,
-	"StartTime"	DateTime NOT NULL,
-	"EndTime"	DATETIME NOT NULL,
-	"Position"	TEXT NOT NULL,
-	"Salary"	INTEGER NOT NULL,
-	FOREIGN KEY("EmployeeID") REFERENCES "Employees"("Id"),
-	PRIMARY KEY("Id" AUTOINCREMENT)
+CREATE TABLE Tickets (
+    Id SERIAL PRIMARY KEY,
+    PurchaseDate DATE NOT NULL,
+    AttractionID INTEGER NOT NULL,
+    FOREIGN KEY (AttractionID) REFERENCES Attractions(id)
 );
 
+CREATE TABLE TicketAttractions (
+    TicketId INTEGER NOT NULL,
+    AttractionID INTEGER NOT NULL,
+    
+    PRIMARY KEY (TicketId, AttractionID),
+    FOREIGN KEY (TicketId) REFERENCES Tickets(Id),
+    FOREIGN KEY (AttractionID) REFERENCES Attractions(id)
+);
+
+CREATE TABLE Employees (
+    Id SERIAL PRIMARY KEY,
+    UserCredentialId INTEGER NOT NULL,
+    FirstName TEXT NOT NULL,
+    LastName TEXT NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    PositionID INTEGER NOT NULL,
+    
+    FOREIGN KEY (UserCredentialId) REFERENCES UserCredentials(Id),
+    FOREIGN KEY (PositionID) REFERENCES Positions(Id)
+);
+
+CREATE TABLE TicketSales (
+    Id SERIAL PRIMARY KEY,
+    TicketId INTEGER NOT NULL,
+    EmployeeId INTEGER NOT NULL,
+    SaleTime TIMESTAMP NOT NULL,
+    
+    FOREIGN KEY (TicketId) REFERENCES Tickets(Id),
+    FOREIGN KEY (EmployeeId) REFERENCES Employees(Id)
+);
---
+++ functions.sql
@@ -0,0 +1,63 @@
+CREATE OR REPLACE FUNCTION check_password() RETURNS trigger AS $$
+BEGIN
+    IF (length(new.password) < 8) THEN
+        RAISE EXCEPTION 'Пароль должен содержать минимум 8 символов';
+    END IF;
+
+    IF (new.password !~ '[A-Za-z0-9]') THEN
+        RAISE EXCEPTION 'Пароль должен содержать буквы и цифры';
+    END IF;
+
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_password
+    BEFORE INSERT OR UPDATE ON UserCredentials
+    FOR EACH ROW
+EXECUTE PROCEDURE check_password();
+
+CREATE OR REPLACE FUNCTION check_unique_login() RETURNS trigger AS $$
+BEGIN
+    IF (EXISTS (SELECT 1 FROM UserCredentials WHERE Login = NEW.Login)) THEN
+        RAISE EXCEPTION 'Логин должен быть уникальным';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_unique_login
+    BEFORE INSERT OR UPDATE ON UserCredentials
+    FOR EACH ROW
+EXECUTE PROCEDURE check_unique_login();
+
+CREATE OR REPLACE FUNCTION check_position_count() RETURNS trigger AS $$
+DECLARE
+    position_count INTEGER;
+    employees_count INTEGER;
+BEGIN
+    IF (NEW.PositionId IS NOT NULL) THEN
+
+        SELECT Count INTO position_count
+        FROM Positions
+        WHERE Id = NEW.PositionId;
+
+        SELECT count(*) INTO employees_count
+        FROM Employees
+        WHERE PositionId = NEW.PositionId;
+
+        IF (employees_count >= position_count) THEN
+            RAISE EXCEPTION 'Превышено максимальное количество сотрудников на позиции %', NEW.PositionId;
+        END IF;
+
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_position_count
+    BEFORE INSERT ON Employees
+    FOR EACH ROW
+EXECUTE PROCEDURE check_position_count();
